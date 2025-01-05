cef4db11416c5dc73c39695623d577cfbb8d60f7
Camille Police <camillepolice@icloud.com>
2025-01-05 17:24:43 +0000

    chore(proxy): fix dependencies

--- proxy/src/auth/auth.module.ts
+++ proxy/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
-import { JwtStrategy } from './jwt.strategy'
+import { JwtStrategy } from './jwt/jwt.strategy'
 import { ConfigService } from '@nestjs/config'
 
 @Module({
